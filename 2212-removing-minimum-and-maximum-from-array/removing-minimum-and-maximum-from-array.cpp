8c0ec3d3bbba51b3558dbf9f5e0882cd0240cbfc
ayushcodes2612 <ayushth147@gmail.com>
2026-08-30 20:36:41 +0530

    Time: 7 ms (10.94%) | Memory: 92.1 MB (58.82%) - LeetSync

--- 2212-removing-minimum-and-maximum-from-array/removing-minimum-and-maximum-from-array.cpp
+++ 2212-removing-minimum-and-maximum-from-array/removing-minimum-and-maximum-from-array.cpp
@@ -15,9 +15,9 @@ public:
                 I2 = i + 1;
             }
         }
-        int left = max(I1, I2);
-        int right = n - min(I1, I2) + 1;
-        int both = min(I1, I2) + n - left + 1;
-        return min({left, right, both});
+        int l = max(I1, I2);
+        int r = n - min(I1, I2) + 1;
+        int b = min(I1, I2) + n - l + 1;
+        return min({l, r, b});
     }
 };
