821c205662abbd00252e21b60d23965793d83a45
ayushcodes2612 <ayushth147@gmail.com>
2026-09-09 12:42:09 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (84.80%) - LeetSync

--- 4248-count-commas-in-range-ii/count-commas-in-range-ii.cpp
+++ 4248-count-commas-in-range-ii/count-commas-in-range-ii.cpp
@@ -3,10 +3,10 @@ public:
     long long countCommas(long long n) {
         if (n < 1000)
             return 0;
-        long long int count = 0;
+        long long int c = 0;
         for (long long int i = 1000; i <= n; i *= 1000)
-            count += n - i + 1;
+            c += n - i + 1;
 
-        return count;
+        return c;
     }
 };
